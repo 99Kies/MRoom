963a9f7e44d8e86c613e0922f777703a094d9249
99Kies <1290017556@qq.com>
2020-03-19 11:34:17 +0800

    update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import current_user, LoginManager
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired, Length
+from werkzeug.security import check_password_hash, generate_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
 
-@app.route("/")
+bootstrap = Bootstrap(app)
+login_manager = LoginManager(app)
+
+password = generate_password_hash("123456")
+
+class UserForm(FlaskForm):
+    userid = PasswordField("User ID", validators=[DataRequired(), Length(8,30)])
+    submit = SubmitField("登入")
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    form = UserForm()
+    if form.validate_on_submit():
+        userid = form.userid.data
+        if check_password_hash(password, "123456"):
+            return redirect(url_for("server"))
+        flash("账号或者密码错误！")
+    return render_template("index.html", form=form)
+
+
+
+@app.route('/server')
+def server():
+    return render_template("server.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0')
